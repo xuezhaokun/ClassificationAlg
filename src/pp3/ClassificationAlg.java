bd9c3d49101228b928a5391d3c5aab18878a22fd
Zhaokun Xue <helloxzk@gmail.com>
2016-11-10 22:46:52 -0500

    add read file functions

---
+++ src/pp3/ClassificationAlg.java
@@ -0,0 +1,83 @@
+package pp3;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import Jama.Matrix;
+
+public class ClassificationAlg {
+	/**
+	 * The function parses an input file to a list of strings
+	 * @param filename input file name
+	 * @return a matrix
+	 * @throws IOException input/output exception
+	 */
+	public static double[][] readData(String filename) throws IOException {
+
+        List<double[]> examples = new ArrayList<double[]>();
+        double[][] phi = null;
+		// try to open and read the file
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(filename));
+            String fileRead = br.readLine();
+            // parse file content by space and add each token to the list
+            while (fileRead != null) {
+                String[] tokens = fileRead.split(",");
+                double[] example = new double[tokens.length];
+                for (int i = 0; i < tokens.length; i++) {
+                		example[i] = Double.parseDouble(tokens[i]);
+                }
+                examples.add(example);
+                fileRead = br.readLine();
+            }
+            phi = new double[examples.size()][0];
+            br.close();
+            
+        } catch (FileNotFoundException fnfe) {
+            System.err.println("file not found");
+        }
+		return examples.toArray(phi);
+	}
+	
+	/**
+	 * The function to read label file
+	 * @param filename label file name
+	 * @return an array contains all labels
+	 * @throws IOException
+	 */
+	public static double[] readLabels(String filename) throws IOException {
+
+        List<Double> labels = new ArrayList<Double>();
+        double[] t = null;
+		// try to open and read the file
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(filename));
+            String fileRead = br.readLine();
+            // parse file content by space and add each token to the list
+            while (fileRead != null) {
+            		labels.add(Double.parseDouble(fileRead));
+                fileRead = br.readLine();
+            }
+            t = new double[labels.size()];
+            for (int i = 0; i < t.length; i++) {
+                t[i] = labels.get(i);
+             }
+            br.close();
+            
+        } catch (FileNotFoundException fnfe) {
+            System.err.println("file not found");
+        }
+		return t;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
